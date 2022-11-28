1096dfc8366417ed8249ccb30e944ea9205a72bf
dyparkkk <dypark@dyParkui-MacBookPro.local>
2022-11-28 18:07:19 +0900

    Add file NetworkManager TODO

---
+++ analysis/NetworkManager.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace Mirror
+{
+  [HelpURL("https://mirror-networking.gitbook.io/docs/components/network-manager")]
+  public class NetworkManager : MonoBehaviour
+  {
+    // TODO : doc 보고 Manager가 하는 역할 분류
+    // 메서드별로 어떤 분류에 들어가는지 정리
+    // roomManager, LobbyManager 관계 정리
+
+    
+  }
+}
